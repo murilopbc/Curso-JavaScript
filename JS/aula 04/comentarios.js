9e51f068c78b98c07a7796629000b91803269025
murilopbc <murilopoltronieri3@gmail.com>
2023-12-19 11:01:30 -0300

    js

---
+++ JS/aula 04/comentarios.js
@@ -0,0 +1,10 @@
+// Comentário de linha
+
+/*
+Comentário para 
+bloco de código
+ */
+
+console.log('Linha 1');
+console.log('Linha 2');
+// console.log('Linha 3');
---
+++ JS/aula 06/index.js
@@ -0,0 +1,7 @@
+let nome; // Declarar Variável
+console.log(nome)
+
+nome = "João"; // Inicializar váriável String
+
+console.log(nome, "nasceu em 2005.");
+console.log("Em 2010", nome, "conheceu Maria.")
