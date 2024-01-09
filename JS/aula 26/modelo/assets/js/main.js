b59c3cdb35e6fef4c78162be7d4c423505691f98
murilopbc <murilopoltronieri3@gmail.com>
2024-01-09 14:18:32 -0300

    js

--- JS/aula 26/modelo/assets/js/main.js
+++ JS/aula 26/modelo/assets/js/main.js
@@ -4,9 +4,9 @@ const form = document.querySelector('#formulario');
 
 form.addEventListener('submit', function (e){
     e.preventDefault();
-    const inputPeso = e.target.querySelector();
+    const inputPeso = e.target.querySelector('#peso');
     console.log(inputPeso);
-    const inputAltura = e.target.querySelector();
+    const inputAltura = e.target.querySelector('#altura');
     console.log(inputAltura);
 });
 
